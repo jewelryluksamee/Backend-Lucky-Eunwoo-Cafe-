11f2d6af8631895a6ed554d18b10fd0dcb8f419e
Jewelry <luksamee.jewelry@gmail.com>
2026-03-17 22:26:36 +0700

    fix: change default server port from 3000 to 80 in server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors    = require('cors');
 const wallRoutes = require('./routes/wall');
 
 const app  = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 
 app.use(cors({ origin: process.env.ALLOWED_ORIGIN || '*' }));
 app.use(express.json());
